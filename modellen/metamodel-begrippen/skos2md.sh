460c8eeaac8a08e9af5831ff6e88659728407aa0
architolk <msj@xs4all.nl>
2025-10-17 10:52:13 +0200

    Toevoeging mogelijkheid om plaatje te maken

--- modellen/metamodel-begrippen/skos2md.sh
+++ modellen/metamodel-begrippen/skos2md.sh
@@ -8,6 +8,12 @@ fi
 if [ ! -f "../../werkomgeving/libs/skos2md.xsl" ]; then
   curl -L -k https://raw.githubusercontent.com/architolk/rdf2xml/refs/heads/main/skos2md.xsl -o ../../werkomgeving/libs/skos2md.xsl
 fi
+if [ ! -f "../../werkomgeving/libs/skos2graphml.xsl" ]; then
+  curl -L -k https://raw.githubusercontent.com/architolk/rdf2xml/refs/heads/main/skos2graphml.xsl -o ../../werkomgeving/libs/skos2graphml.xsl
+fi
 
 # Make markdown from skos
 java -jar ../../werkomgeving/libs/rdf2xml.jar begrippen.ttl ../../metamodel-begrippen.md ../../werkomgeving/libs/skos2md.xsl
+
+# Create diagram
+java -jar ../../werkomgeving/libs/rdf2xml.jar begrippen.ttl begrippen.graphml ../../werkomgeving/libs/skos2graphml.xsl
