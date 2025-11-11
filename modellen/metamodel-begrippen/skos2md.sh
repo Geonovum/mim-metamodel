26a29f8a0d61ca036367ab2990fee1a59898c52a
architolk <msj@xs4all.nl>
2025-11-11 11:44:29 +0100

    Added XMI export of begrippen

--- modellen/metamodel-begrippen/skos2md.sh
+++ modellen/metamodel-begrippen/skos2md.sh
@@ -11,9 +11,15 @@ fi
 if [ ! -f "../../werkomgeving/libs/skos2graphml.xsl" ]; then
   curl -L -k https://raw.githubusercontent.com/architolk/rdf2xml/refs/heads/main/skos2graphml.xsl -o ../../werkomgeving/libs/skos2graphml.xsl
 fi
+if [ ! -f "../../werkomgeving/libs/skos2xmi.xsl" ]; then
+  curl -L -k https://raw.githubusercontent.com/architolk/rdf2xml/refs/heads/main/skos2xmi.xsl -o ../../werkomgeving/libs/skos2xmi.xsl
+fi
 
 # Make markdown from skos
 java -jar ../../werkomgeving/libs/rdf2xml.jar begrippen.ttl ../../metamodel-begrippen.md ../../werkomgeving/libs/skos2md.xsl
 
 # Create diagram
 java -jar ../../werkomgeving/libs/rdf2xml.jar begrippen.ttl begrippen.graphml ../../werkomgeving/libs/skos2graphml.xsl
+
+# Create XMI export
+java -jar ../../werkomgeving/libs/rdf2xml.jar begrippen.ttl begrippen.xmi ../../werkomgeving/libs/skos2xmi.xsl
